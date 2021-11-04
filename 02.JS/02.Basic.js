620efb546569b6b31d14eee74e61d7048e18013d
Cheolkie Kim <ckiekim@naver.com>
2021-11-04 22:17:03 +0900

    자바스크립트

---
+++ 02.JS/02.Basic.js
@@ -0,0 +1,16 @@
+/*
+ * These are multiline comments.
+ */
+console.log('안녕' + "하세요?");
+let s = '안녕' + "하세요?";
+
+// Template literal
+for (let i=1; i <= 9; i++) {
+    console.log(`4 x ${i} = ${4*i}`);
+}
+
+let sum = 0
+for (let i=1; i<=100; i++) {
+    sum += i;
+}
+console.log(sum);
