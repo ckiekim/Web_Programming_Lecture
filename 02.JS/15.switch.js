61714e05ec6430c0925ddd31311b762e4692fd57
Cheolkie Kim <ckiekim@naver.com>
2021-11-05 19:51:33 +0900

    조건문

--- 02.JS/15.switch.js
+++ 02.JS/15.switch.js
@@ -2,6 +2,7 @@ let score = 85;
 let grade;
 
 switch (Math.floor(score / 10)) {
+    case 10:
     case 9:
         grade = 'A';
         break;
@@ -16,5 +17,18 @@ switch (Math.floor(score / 10)) {
         break;
     default:
         grade = 'F';
+}
+console.log(grade);
+
+if (score >= 90) {
+    grade = 'A';
+} else if (score >= 80) {
+    grade = 'B';
+} else if (score >= 70) {
+    grade = 'C';
+} else if (score >= 60) {
+    grade = 'D';
+} else {
+    grade = 'F';
 }
 console.log(grade);
---
+++ 02.JS/16.삼항연산자.js
@@ -0,0 +1,12 @@
+let number = 15;
+/*let result;
+
+if (number % 2 == 0) {
+    result = 'even';
+} else {
+    result = 'odd';
+}
+console.log(result);*/
+
+let result = number % 2 == 0 ? 'even' : 'odd';
+console.log(result);
